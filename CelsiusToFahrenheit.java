1a553e670dd901b44f6c2eb276f2f90936e8a3f3
TawqPlatform <151886835+TawqPlatform@users.noreply.github.com>
2023-11-24 03:08:25 +0300

    Update CelsiusToFahrenheit.java

--- CelsiusToFahrenheit.java
+++ CelsiusToFahrenheit.java
@@ -11,10 +11,10 @@ public class CelsiusToFahrenheit {
     public static void main(String[] Strings) {
         // declare a variable to store the celsius degree
         double celsius = 100;
-        // convert it using the formela in the question
+        // convert it using the formula in the question
         double  fahrenheit = (9 * celsius + 32 * 5) / 5;
         
-        // display the result exactlly as expected
+        // display the result exactly as expected
         System.out.println(fahrenheit + " degree Fahrenheit is equal to " + celsius + " in Celsius");
     }
 }
